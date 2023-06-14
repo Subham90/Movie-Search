7eb760c9dbb7c60ed0d5bd6ff644fcd5a7d96fd3
Subham90 <sg.21u10083@btech.nitdgp.ac.in>
2023-06-14 08:13:14 +0530

    second-commit

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9204165f`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9204165f`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
             displayMovieDetails(movieDetails);
